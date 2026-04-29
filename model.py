6c24c822fb2d304454698f7ad65e5413a0d2c28b
Artem61732 <artem680v194@gmail.com>
2026-04-29 17:49:48 +0900

    change all

---
+++ model.py
@@ -0,0 +1,31 @@
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import root_mean_squared_error, r2_score, root_mean_squared_log_error, mean_absolute_error
+
+test_df = pd.read_csv('data/test.csv')
+train_df = pd.read_csv('data/train.csv')
+
+X = train_df.drop('SalePrice', axis=1)
+X = X.select_dtypes(include=['number'])
+X = X.fillna(0)
+
+y = train_df['SalePrice']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+model = LinearRegression()
+
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+mae = mean_absolute_error(y_test, y_pred)
+rmse = root_mean_squared_error(y_test, y_pred)
+rmsle = root_mean_squared_log_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
+
+print(f'Mean Absolute Error: {mae}')
+print(f'Root Mean Squared Error: {rmse}')
+print(f'R2 Score: {r2}')
+print(f'Root Mean Squared Log Error: {rmsle}')
