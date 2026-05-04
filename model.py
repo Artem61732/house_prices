a6fd47ce264ecd257a1ad281ff665e3ac4869a67
Artem61732 <artem680v194@gmail.com>
2026-05-04 17:49:49 +0900

    score 0.131 with catboost

--- model.py
+++ model.py
@@ -1,29 +1,61 @@
 import pandas as pd
 import numpy as np
+
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import OneHotEncoder
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.linear_model import LinearRegression, Ridge
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import root_mean_squared_error, r2_score, root_mean_squared_log_error, mean_absolute_error
-from sklearn.impute import SimpleImputer
+
+from xgboost import XGBRegressor
+
+from catboost import CatBoostRegressor
 
 test_df = pd.read_csv('data/test.csv')
 train_df = pd.read_csv('data/train.csv')
-simpleImputer = SimpleImputer(missing_values = np.nan)
+
+def feature_engineering(train_df):
+    data = train_df.copy()
+    data = data.drop('Id', axis=1)
+    return data
 
 X = train_df.drop('SalePrice', axis=1)
-X = pd.get_dummies(X)
-X = simpleImputer.fit_transform(X)
-
 y = train_df['SalePrice']
+
+X = feature_engineering(X)
+
+numeric_features = X.select_dtypes(include=['int64', 'float64']).columns
+categorical_features = X.select_dtypes(include=['object']).columns
+
+numeric_transformer = Pipeline(steps=[
+    ('imputer', SimpleImputer(missing_values = np.nan))
+])
+
+categorical_transformer = Pipeline(steps=[
+    ('imputer', SimpleImputer(strategy='most_frequent')),
+    ('onehot', OneHotEncoder(handle_unknown='ignore'))
+])
+
+preprocessor = ColumnTransformer(
+    transformers=[
+        ('num', numeric_transformer, numeric_features),
+        ('cat', categorical_transformer, categorical_features)
+    ]
+)
+
+pipeline = Pipeline(steps=[
+    ('preprocessor', preprocessor),
+    ('model', CatBoostRegressor(random_state=42))
+])
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-model = RandomForestRegressor()
+pipeline.fit(X_train, y_train)
 
-model.fit(X_train, y_train)
-
-y_pred = model.predict(X_test)
-y_pred = np.maximum(0, y_pred)
+y_pred = pipeline.predict(X_test)
 
 mae = mean_absolute_error(y_test, y_pred)
 rmse = root_mean_squared_error(y_test, y_pred)
